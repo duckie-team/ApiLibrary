641a72c520786079c7633ba47e072a00a2f6ff58
jisungbin <ji@sungb.in>
2022-10-27 03:49:18 +0900

    fix build error

--- presentation/src/main/kotlin/land/sungbin/apilibrary/MainActivity.kt
+++ presentation/src/main/kotlin/land/sungbin/apilibrary/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
