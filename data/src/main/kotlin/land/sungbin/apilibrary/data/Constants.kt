879e1a70029a7627acf9d53d07b14abab4895c8c
jisungbin <ji@sungb.in>
2022-10-29 08:44:12 +0900

    feat: request 에 공통되는 상수 파일 추가

---
+++ data/src/main/kotlin/land/sungbin/apilibrary/data/Constants.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
+
+package land.sungbin.apilibrary.data
+
+internal object Constants {
+    const val MaxTimeoutMs = 3000L
+    const val MaxRetryCount = 3
+    const val ApiUrl = "https://api.publicapis.org/entries"
+}
