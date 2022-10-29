da15cafe1254ec647325b8fb95a3776795275044
jisungbin <ji@sungb.in>
2022-10-29 17:14:35 +0900

    feat: MarqueeText 컴포저블 추가

---
+++ presentation/src/main/kotlin/land/sungbin/apilibrary/presentation/ui/MarqueeText.kt
@@ -0,0 +1,156 @@
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
+
+package land.sungbin.apilibrary.presentation.ui
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.TargetBasedAnimation
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import kotlinx.coroutines.delay
+
+// https://stackoverflow.com/a/68980032/14299073
+// edited: remove gradient shadow (color rendering issue; only white drawing)
+@Composable
+internal fun MarqueeText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textModifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current,
+) {
+    val createText = @Composable { localModifier: Modifier ->
+        Text(
+            text = text,
+            textAlign = textAlign,
+            modifier = localModifier,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = 1,
+            onTextLayout = onTextLayout,
+            style = style,
+        )
+    }
+    var offset by remember { mutableStateOf(0) }
+    val textLayoutInfoState = remember { mutableStateOf<TextLayoutInfo?>(null) }
+    LaunchedEffect(textLayoutInfoState.value) {
+        val textLayoutInfo = textLayoutInfoState.value ?: return@LaunchedEffect
+        if (textLayoutInfo.textWidth <= textLayoutInfo.containerWidth) return@LaunchedEffect
+        val duration = 7500 * textLayoutInfo.textWidth / textLayoutInfo.containerWidth
+        val delay = 1000L
+
+        do {
+            val animation = TargetBasedAnimation(
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = duration,
+                        delayMillis = 1000,
+                        easing = LinearEasing,
+                    ),
+                    repeatMode = RepeatMode.Restart
+                ),
+                typeConverter = Int.VectorConverter,
+                initialValue = 0,
+                targetValue = -textLayoutInfo.textWidth
+            )
+            val startTime = withFrameNanos { it }
+            do {
+                val playTime = withFrameNanos { it } - startTime
+                offset = (animation.getValueFromNanos(playTime))
+            } while (!animation.isFinishedFromNanos(playTime))
+            delay(delay)
+        } while (true)
+    }
+
+    SubcomposeLayout(
+        modifier = modifier.clipToBounds()
+    ) { constraints ->
+        val infiniteWidthConstraints = constraints.copy(maxWidth = Int.MAX_VALUE)
+        var mainText = subcompose(MarqueeLayers.MainText) {
+            createText(textModifier)
+        }.first().measure(infiniteWidthConstraints)
+
+        var secondPlaceableWithOffset: Pair<Placeable, Int>? = null
+        if (mainText.width <= constraints.maxWidth) {
+            mainText = subcompose(MarqueeLayers.SecondaryText) {
+                createText(textModifier.fillMaxWidth())
+            }.first().measure(constraints)
+            textLayoutInfoState.value = null
+        } else {
+            val spacing = constraints.maxWidth * 2 / 3
+            textLayoutInfoState.value = TextLayoutInfo(
+                textWidth = mainText.width + spacing,
+                containerWidth = constraints.maxWidth
+            )
+            val secondTextOffset = mainText.width + offset + spacing
+            val secondTextSpace = constraints.maxWidth - secondTextOffset
+            if (secondTextSpace > 0) {
+                secondPlaceableWithOffset = subcompose(MarqueeLayers.SecondaryText) {
+                    createText(textModifier)
+                }.first().measure(infiniteWidthConstraints) to secondTextOffset
+            }
+        }
+
+        layout(
+            width = constraints.maxWidth,
+            height = mainText.height
+        ) {
+            mainText.place(offset, 0)
+            secondPlaceableWithOffset?.let {
+                it.first.place(it.second, 0)
+            }
+        }
+    }
+}
+
+private enum class MarqueeLayers { MainText, SecondaryText; }
+private data class TextLayoutInfo(val textWidth: Int, val containerWidth: Int)
