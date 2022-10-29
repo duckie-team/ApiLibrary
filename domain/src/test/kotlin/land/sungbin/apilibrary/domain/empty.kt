9e7f9d6dd43f1db61683308b9526eb7f5d1f0a74
jisungbin <ji@sungb.in>
2022-10-29 23:14:11 +0900

    test: 테스트 폴더 생성

--- domain/src/test/kotlin/land/sungbin/apilibrary/domain/empty.kt
+++ domain/src/test/kotlin/land/sungbin/apilibrary/domain/empty.kt
@@ -1,3 +1,8 @@
-package land.sungbin.apilibrary.data
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
 
-// TODO!
+package land.sungbin.apilibrary.domain
