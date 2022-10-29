67432fb795b1e8d0302adc770edcabd7c669262e
jisungbin <ji@sungb.in>
2022-10-29 17:14:12 +0900

    feat: Browser 유틸 추가

---
+++ presentation/src/main/kotlin/land/sungbin/apilibrary/presentation/util/Browser.kt
@@ -0,0 +1,36 @@
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
+
+package land.sungbin.apilibrary.presentation.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.annotation.ColorInt
+import androidx.browser.customtabs.CustomTabsIntent
+
+
+object Browser {
+    private val customTabsIntent = CustomTabsIntent.Builder().build()
+
+    fun open(
+        context: Context,
+        url: Uri,
+    ) {
+        val pm = context.packageManager
+        val intent = pm.getLaunchIntentForPackage("com.android.chrome")
+        if (intent == null) {
+            val createChooser = Intent.createChooser(
+                Intent(Intent.ACTION_VIEW, url),
+                "Open with",
+            )
+            context.startActivity(createChooser)
+        } else {
+            customTabsIntent.launchUrl(context, url)
+        }
+    }
+}
