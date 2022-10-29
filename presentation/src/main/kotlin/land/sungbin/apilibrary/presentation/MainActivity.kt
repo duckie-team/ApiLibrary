5d557c40f8e50341c14918fc646d998f04c7a9d4
jisungbin <ji@sungb.in>
2022-10-29 14:03:53 +0900

    feat: 메인 화면 구성

--- presentation/src/main/kotlin/land/sungbin/apilibrary/presentation/MainActivity.kt
+++ presentation/src/main/kotlin/land/sungbin/apilibrary/presentation/MainActivity.kt
@@ -5,33 +5,55 @@
  * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
  */
 
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package land.sungbin.apilibrary.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import land.sungbin.apilibrary.presentation.theme.ApiLibraryTheme
+import land.sungbin.apilibrary.presentation.ui.ApiList
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     @Inject
-    lateinit var vm: MainViewModel
+    internal lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Bye world: ${vm.hashCode()}")
+            ApiLibraryTheme {
+                val uiState by vm.state.collectAsState()
+
+                LaunchedEffect(key1 = Unit) {
+                    vm.loadApis()
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { padding ->
+                    ApiList(
+                        modifier = Modifier.padding(
+                            paddingValues = padding,
+                        ),
+                        uiState = uiState,
+                    )
+                }
             }
         }
     }
