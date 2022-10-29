5b7f4e8a0e3f0c000d7b08126f36725d66659219
jisungbin <ji@sungb.in>
2022-10-29 14:00:50 +0900

    feat: ApiItem 도메인 모델 필드 정리 및 문서화

--- domain/src/main/kotlin/land/sungbin/apilibrary/domain/model/ApiItem.kt
+++ domain/src/main/kotlin/land/sungbin/apilibrary/domain/model/ApiItem.kt
@@ -7,12 +7,23 @@
 
 package land.sungbin.apilibrary.domain.model
 
+/**
+ * Api 아이템 정보 객체
+ *
+ * @param name API 이름
+ * @param description API 설명
+ * @param category API 카테고리
+ * @param link API 홈페이지
+ * @param neededAuth 필요한 인증키
+ * @param supportCORS CORS 지원 여부
+ */
 data class ApiItem(
-    val description: String,
-    val category: String,
-    val https: Boolean,
-    val auth: String,
-    val api: String,
-    val cors: String,
+    val name: String,
+    val description: String?,
+    val category: String?,
     val link: String,
-)
+    val neededAuth: String?,
+    val supportCORS: Boolean,
+) {
+    companion object
+}
