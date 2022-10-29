30426103de1f135446f5e70f19a769cd196fce4d
jisungbin <ji@sungb.in>
2022-10-29 23:14:08 +0900

    test: 테스트 폴더 생성

---
+++ presentation/src/androidTest/kotlin/land/sungbin/apilibrary/presentation/empty.kt
@@ -0,0 +1,8 @@
+/*
+ * Designed and developed by 2022 Ji Sungbin.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/ApiLibrary/blob/trunk/LICENSE
+ */
+
+package land.sungbin.apilibrary.presentation
